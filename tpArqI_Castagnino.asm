2c2a98d30d23c8c192ccc9b5e7d4db54f3e43b6e
tadeo <tadeocastagnino@gmail.com>
2024-11-23 19:50:57 -0300

    Actualizacion

--- tpArqI_Castagnino.asm
+++ tpArqI_Castagnino.asm
@@ -1,4 +1,24 @@
 	.data
-palabra1:	.word 0x10
-	.text
-main:	lw $s0, palabra1
+slist:  .word 0
+cclist:	.word 0
+wclist:	.word 0
+schedv: .space 32
+menu:	.ascii "Colecciones de objetos categorizados\n"
+	.ascii "====================================\n"
+	.ascii "1-Nueva categoria\n"
+	.ascii "2-Siguiente categoria\n"
+	.ascii "3-Categoria anterior\n"
+	.ascii "4-Listar categorias\n"
+	.ascii "5-Borrar categoria actual\n"
+	.ascii "6-Anexar objeto a la categoria actual\n"
+	.ascii "7-Listar objetos de la categoria\n"
+	.ascii "8-Borrar objeto de la categoria\n"
+	.ascii "0-Salir\n"
+	.asciiz "Ingrese la opcion deseada: "
+error:	.asciiz "Error: "
+return:	.asciiz "\n"
+catName:.asciiz "\nIngrese el nombre de una categoria: "
+selCat:	.asciiz "\nSe ha seleccionado la categoria:"
+idObj:	.asciiz "\nIngrese el ID del objeto a eliminar: "
+objName:.asciiz "\nIngrese el nombre de un objeto: "
+success:.asciiz "La operación se realizo con exito\n\n"
