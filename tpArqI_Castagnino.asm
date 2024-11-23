2bd616e44dd33b88bd6edea9bad4585bcdbfebae
tadeo <tadeocastagnino@gmail.com>
2024-11-23 20:21:39 -0300

    Actualizacion

--- tpArqI_Castagnino.asm
+++ tpArqI_Castagnino.asm
@@ -22,3 +22,39 @@ selCat:	.asciiz "\nSe ha seleccionado la categoria:"
 idObj:	.asciiz "\nIngrese el ID del objeto a eliminar: "
 objName:.asciiz "\nIngrese el nombre de un objeto: "
 success:.asciiz "La operación se realizo con exito\n\n"
+
+error2: .asciiz "Error: Opcion invalida\n"
+
+	.text 0x00400000   # Dirección inicial para código
+main:
+    jal print_menu         # Imprimir el menú
+    jal get_option         # Leer la opción ingresada por el usuario
+    jal validate_option
+    
+    li $v0, 10             # Exit syscall
+    syscall
+
+print_menu:
+    la $a0, menu           # Cargar la dirección del menú
+    li $v0, 4              # Syscall para imprimir string
+    syscall
+
+get_option:
+    li $v0, 5             # Syscall para leer un entero
+    syscall
+    move $t0, $v0         # Guardar el valor leído en $t0
+    jr $ra                # Retornar a main
+    
+validate_option:
+    bltz $t0, invalid     # Si el valor es menor que 0, es inválido
+    li $t1, 8
+    bgt $t0, $t1, invalid # Si el valor es mayor que 8, es inválido
+    jr $ra                # Si es válido, retorna a main
+
+invalid:
+    la $a0, error2         # Cargar el mensaje de error
+    li $v0, 4             # Syscall para imprimir string
+    syscall
+    jal print_menu        # Volver a imprimir el menú
+    jr $ra                # Retornar a main
+
